50452b125a720c049212b09fbd7a7eefe67c5e2f
shisonghua <shisonghua@gmail.com>
2020-03-04 03:36:33 +0800

    Add file open dialog

--- PythonScript/PUBG_QA/RenameBugPic.py
+++ PythonScript/PUBG_QA/RenameBugPic.py
@@ -2,9 +2,11 @@ import os
 import re
 import shutil
 import openpyxl
-cwd = os.getcwd()
-#wbdd = openpyxl.load_workbook('c:\\Users\\shiso\\Documents\\Repo\\20181122\\PythonScript\\PUBG_QA\\PUBG_QA.XLSX')
-wb = openpyxl.load_workbook(cwd+'\PUBG_QA\PUBG_QA.XLSX', data_only = 1)
+import tkinter.filedialog
+#wb = openpyxl.load_workbook('c:\\Users\\shiso\\Documents\\Repo\\20181122\\PythonScript\\PUBG_QA\\PUBG_QA.XLSX')
+#cwd = os.getcwd()
+#wb = openpyxl.load_workbook(cwd+'\PUBG_QA\PUBG_QA.XLSX', data_only = 1)
+wb =openpyxl.load_workbook(tkinter.filedialog.askopenfilename(title='Choose Excel File...'), data_only = 1)
 sheet = wb['ShiSonghua']
 row_count = sheet.max_row
 KeyDic = {}
@@ -14,7 +16,8 @@ for i in range(2,row_count+1):
     if (myKey!=None) & (jiraKey!=None):
         KeyDic[myKey] = jiraKey
         #print(str(myKey) + '  ' + str(jiraKey))
-picDir = 'C:\\Users\\shiso\\Documents\\Virtuos\\PUBG_QA\\test_files'
+#picDir = 'C:\\Users\\shiso\\Documents\\Virtuos\\PUBG_QA\\test_files'
+picDir = tkinter.filedialog.askdirectory(title='Choose Pic Folder...')
 oldFileNameList = os.listdir(picDir)
 for name in oldFileNameList:
     if '.' in name:
@@ -25,8 +28,6 @@ for name in oldFileNameList:
                 newName = 'PUBG_'+KeyDic[key]+'.'+fExt
                 print(str(key)+' >> '+newName)
                 os.chdir(picDir)
-                #Rename and replace
-                shutil.move(name,newName)
-                #Rename + Copy
-                #newName = picDir+'\\NEW\\'+newName
+                shutil.move(name,newName)                       #Rename and replace
+                #newName = picDir+'\\NEW\\'+newName             #Rename + Copy
                 #shutil.copyfile(name,newName)
