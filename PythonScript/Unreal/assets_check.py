b7016cf7ed304b02a000fe5124ca55649fc5d92b
shisonghua <4364117+pto2k@users.noreply.github.com>
2020-03-31 03:50:29 +0800

    unreal asseting naming fix

--- PythonScript/Unreal/assets_check.py
+++ PythonScript/Unreal/assets_check.py
@@ -1,96 +1,113 @@
 import unreal
-
-workingPath = "/Game/"
-
-@unreal.uclass()
-class GetEditorAssetLibrary(unreal.EditorAssetLibrary):
-    pass
-
-editorAssetLib = GetEditorAssetLibrary();
-
-#all_asset_string = editorAssetLib.list_assets(workingPath, True, False)
-all_asset_string = unreal.EditorAssetLibrary.list_assets(workingPath, True, False)
-num_target_assets=len(all_asset_string)
-unreal.log('Total assets count = '+ str(num_target_assets))
-#print help(editorAssetLib)
-
-temp_data = unreal.EditorAssetLibrary.find_asset_data(all_asset_string[0])
-print dir(temp_data)
-
-print ("asset_class = " + str(temp_data.asset_class))
-print ("asset_name = " + str(temp_data.asset_name))
-#print ("assign = " + temp_data.assign())
-# print ("cast = " + temp_data.cast())
-# print ("copy = " + temp_data.copy())
-# print ("get_asset = " + temp_data.get_asset())
-# print ("get_class = " + temp_data.get_class())
-# print ("get_editor_property = " + temp_data.get_editor_property())
-print ("get_export_text_name = " + temp_data.get_export_text_name())
-print ("get_full_name = " + temp_data.get_full_name())
-# print ("get_tag_value = " + temp_data.get_tag_value())
-print ("is_asset_loaded = " + str(temp_data.is_asset_loaded()))
-print ("is_redirector = " + str(temp_data.is_redirector()))
-print ("is_u_asset = " + str(temp_data.is_u_asset()))
-print ("is_valid = " + str(temp_data.is_valid()))
-print ("object_path = " + str(temp_data.object_path))
-print ("package_name = " + str(temp_data.package_name))
-print ("package_path = " + str(temp_data.package_path))
-#print ("set_editor_property = " + temp_data.set_editor_property())
-print ("static_struct = " + str(temp_data.static_struct()))
-print ("to_soft_object_path = " + str(temp_data.to_soft_object_path()))
-print ("to_tuple = " + str(temp_data.to_tuple()))
+import re
 
 
 referenced_assets = []
 non_referenced_assets = []
+throwable_sm_assets = []
 throwable_bp_assets = []
 
+def logit0(info):
+    unreal.log(info)
 
-with unreal.ScopedSlowTask(num_target_assets) as slow_find_task:
-    slow_find_task.make_dialog(True)
-    for asset_string in all_asset_string:
-        asset_data = unreal.EditorAssetLibrary.find_asset_data(asset_string)
+def logit(info):
+    unreal.log_warning(info)
 
-        if (asset_data.is_valid() and not asset_data.is_redirector()):
-            # Find referencers
-            referencers = unreal.EditorAssetLibrary.find_package_referencers_for_asset(asset_string, False)
+def logit2(info):
+    unreal.log_error(info)
+
+all_lib_assets = unreal.EditorAssetLibrary.list_assets("/Game/", True, False)
+num_all_assets = len(all_lib_assets)
+logit('Total assets count in library = '+ str(num_all_assets))
+all_level_actors = unreal.EditorLevelLibrary.get_all_level_actors()
+
+
+def check_lib_obj(index):
+    temp_data = unreal.EditorAssetLibrary.find_asset_data(all_lib_assets[index])
+    #print dir(temp_data)
+    logit(">>>>>>TEMP DATA INFO START>>>>>>>>>>>")
+    logit("asset_class = " + str(temp_data.asset_class))
+    logit("asset_name = " + str(temp_data.asset_name))
+    #logit("assign = " + temp_data.assign())
+    #logit("cast = " + temp_data.cast())
+    #logit("copy = " + temp_data.copy())
+    #logit("get_asset = " + temp_data.get_asset())
+    #logit("get_class = " + temp_data.get_class())
+    #logit("get_editor_property = " + temp_data.get_editor_property())
+    logit("get_export_text_name = " + temp_data.get_export_text_name())
+    logit("get_full_name = " + temp_data.get_full_name())
+    # logit("get_tag_value = " + temp_data.get_tag_value())
+    logit("is_asset_loaded = " + str(temp_data.is_asset_loaded()))
+    logit("is_redirector = " + str(temp_data.is_redirector()))
+    logit("is_u_asset = " + str(temp_data.is_u_asset()))
+    logit("is_valid = " + str(temp_data.is_valid()))
+    logit("object_path = " + str(temp_data.object_path))
+    logit("package_name = " + str(temp_data.package_name))
+    logit("package_path = " + str(temp_data.package_path))
+    #logit("set_editor_property = " + temp_data.set_editor_property())
+    logit("static_struct = " + str(temp_data.static_struct()))
+    logit("to_soft_object_path = " + str(temp_data.to_soft_object_path()))
+    logit("to_tuple = " + str(temp_data.to_tuple()))
+    logit(">>>>>>TEMP DATA INFO FINISH>>>>>>>>>>>")
+
+#check_lib_obj(1)
+
+def check_reference():
+    with unreal.ScopedSlowTask(num_all_assets) as slow_find_task:
+        slow_find_task.make_dialog(True)
+        for asset_string in all_lib_assets:
+            asset_data = unreal.EditorAssetLibrary.find_asset_data(all_lib_assets[0])
+            if (asset_data.is_valid() and not asset_data.is_redirector()):
+                # Find referencers
+                referencers = unreal.EditorAssetLibrary.find_package_referencers_for_asset(asset_string, False)
+                if(len(referencers) > 0):
+                    referenced_assets.append(asset_string)
+                else:
+                    non_referenced_assets.append(asset_string)
+            # if pressed "Cancel" button on dialog, task is cancel.
+            if slow_find_task.should_cancel():
+                print("Task cancelled.")
+                del referenced_assets[:]
+                del non_referenced_assets[:]
+                break
+
+            # Advance progress
+            slow_find_task.enter_progress_frame(1, asset_string)
             
-            if(len(referencers) > 0):
-                referenced_assets.append(asset_string)
+    # Display
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print("[Found library assets] " + str(num_all_assets))
+    print("[referenced assets] " + str(len(referenced_assets)))
+    '''for asset_string in referenced_assets:
+        print("    " + asset_string)
+    print("---------------------")
+    print("[Non referenced assets] " + str(len(non_referenced_assets)))
+    for asset_string in non_referenced_assets:
+        print("    " + asset_string)'''
+    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
+def rename_level_throwable_label():
+    p = re.compile('\d\d\d.*ThrowableBreakable')
+    p1 = re.compile('.*ThrowableBreakable')
+    p2 = re.compile('<type \'Actor\'>')
+    
+    for actor in all_level_actors:
+        if (len((p1.findall(actor.get_full_name())))>0):
+            logit("Object Name = " + actor.get_name() + " / type = " + str(type(actor)))
+            if (len(p2.findall(str(type(actor))))):
+                if str(actor.get_actor_label()).startswith('S_'):
+                    logit(str(actor.get_fname()) + " is Actor but With Wrong Prefix")
+                    old_name = str(actor.get_actor_label())
+                    new_name = old_name.replace('S_','BP_')
+                    actor.set_actor_label(new_name)
+                    logit0(old_name + ' --> ' + new_name + ' done.')
+                if str(actor.get_actor_label()).startswith('SM_'):
+                    logit(str(actor.get_fname()) + " is Actor but With Wrong Prefix")
+                    old_name = str(actor.get_actor_label())
+                    new_name = old_name.replace('SM_','BP_')
+                    actor.set_actor_label(new_name)
+                    logit0(old_name + ' --> ' + new_name + ' done.')
             else:
-                non_referenced_assets.append(asset_string)
-        # if pressed "Cancel" button on dialog, task is cancel.
-        if slow_find_task.should_cancel():
-            print("Task cancelled.")
-            del referenced_assets[:]
-            del non_referenced_assets[:]
-            break
+                logit2('Warning '+ str(actor.get_fname()) + " is not actor but in ThrowableBreakable!")
 
-        # Advance progress
-        slow_find_task.enter_progress_frame(1, asset_string)
-        
-# Display
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print("[Find assets] " + str(num_target_assets))
-print("[referenced assets] " + str(len(referenced_assets)))
-'''for asset_string in referenced_assets:
-    print("    " + asset_string)
-print("---------------------")
-print("[Non referenced assets] " + str(len(non_referenced_assets)))
-for asset_string in non_referenced_assets:
-    print("    " + asset_string)'''
-
-print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-'''
-if (len(all_asset_string) > 0):
-    for asset in all_asset_string:
-        deps = editorAssetLib.find_package_referencers_for_asset(asset, False)
-        print type(asset)
-        print type(deps[0])
-        print (deps[0])
-        print(deps[0].get_name())
-        if (len(deps) == 0):
-            print ">>>%s" % asset
-            '''
-
-
+rename_level_throwable_label()
